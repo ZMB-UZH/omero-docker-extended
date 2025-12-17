aff51f95789fff6d2af44662378ec10c29b4efed
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-17 16:57:08 +0100

    Update index_view.py

--- views/index_view.py
+++ views/index_view.py
@@ -160,8 +160,8 @@ def index(request, conn=None, url=None, **kwargs):
                     <hr>
 
                     <div id='var-config' data-var-count='{max_vars}'>
-                        <div style='display:flex; gap:5px; align-items:flex-start; flex-wrap:nowrap;'>
-                            <div style='flex:0 0 350px;'>
+                        <div style='display:flex; gap:5px; align-items:flex-start; '>
+                            <div style='flex:0 0 300px;'>
                                 <label style="font-size:12px; display:block; margin-bottom:6px;">
                                     Variable names
                                 </label>
@@ -176,7 +176,7 @@ def index(request, conn=None, url=None, **kwargs):
                             </div>
                             
                             <div id='variable-set-block'
-                                 style='display:flex; flex-direction:column; gap:15px;'>
+                                style="display:flex; flex-direction:column; gap:15px; flex:1 1 auto; min-width:0;">
 
                                 <div>
                                     <label for='variable_set_select'
@@ -375,6 +375,7 @@ def index(request, conn=None, url=None, **kwargs):
                                  box-shadow: inset 0 0 0 1px #007bff; box-sizing: border-box;}}
                     .omeroweb-filenamemetadata, .omeroweb-filenamemetadata * {{font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}}
                     .nowrap-1 {{white-space: nowrap; overflow: hidden; text-overflow: ellipsis;}}
+                    #variable-set-block * { min-width: 0;}
                     .clamp-2 {{display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2; overflow: hidden; text-overflow: ellipsis;
                                word-break: break-word; white-space: normal; overflow-wrap: anywhere;}}
                     .ui-faded {{opacity: 0.3; filter: grayscale(100%); pointer-events: none;}}
