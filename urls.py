afe51a8e78dcfeefddd912b14366ce302e2fe5e8
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-17 09:18:32 +0100

    Add per-user variable name database storage

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from .views.index_view import index
 from .views.job_view import start_job, job_progress, start_acq_job
 from .views.delete_all_view import delete_all_metadata
 from .views.delete_plugin_view import delete_plugin_metadata
+from .views.variable_set_view import list_sets, save_set, load_set
 
 urlpatterns = [
     path("", index, name="omeroweb_filenamemetadata_index"),
@@ -12,5 +13,7 @@ urlpatterns = [
     path("start_acq_job/", start_acq_job, name="omeroweb_filenamemetadata_start_acq_job"),
     path("delete_all/", delete_all_metadata, name="omeroweb_filenamemetadata_delete_all"),
     path("delete_plugin/", delete_plugin_metadata, name="omeroweb_filenamemetadata_delete_plugin"),
+    path("varsets/", list_sets, name="omeroweb_filenamemetadata_list_sets"),
+    path("varsets/save/", save_set, name="omeroweb_filenamemetadata_save_set"),
+    path("varsets/load/", load_set, name="omeroweb_filenamemetadata_load_set"),
 ]
-
