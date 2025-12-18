b44dd5f4dd90b28be4538a188a579fdccd42e8b6
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-18 15:25:35 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.index_view import index
-from .views.job_view import start_job, job_progress, start_acq_job
+from .views.job_view import start_job, job_progress, start_acq_job, start_delete_all_job, start_delete_plugin_job
 from .views.delete_all_view import delete_all_metadata
 from .views.delete_plugin_view import delete_plugin_metadata
 from .views.variable_set_view import list_sets, save_set, load_set, delete_set
@@ -11,6 +11,8 @@ urlpatterns = [
     path("start_job/", start_job, name="omeroweb_filenamemetadata_start_job"),
     path("progress/<str:job_id>/", job_progress, name="omeroweb_filenamemetadata_job_progress"),
     path("start_acq_job/", start_acq_job, name="omeroweb_filenamemetadata_start_acq_job"),
+    path("start_delete_all_job/", start_delete_all_job, name="omeroweb_filenamemetadata_start_delete_all_job"),
+    path("start_delete_plugin_job/", start_delete_plugin_job, name="omeroweb_filenamemetadata_start_delete_plugin_job"),
     path("delete_all/", delete_all_metadata, name="omeroweb_filenamemetadata_delete_all"),
     path("delete_plugin/", delete_plugin_metadata, name="omeroweb_filenamemetadata_delete_plugin"),
     path("varsets/", list_sets, name="omeroweb_filenamemetadata_list_sets"),
