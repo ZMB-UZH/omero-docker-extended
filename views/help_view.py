236a603e6e1e8d9c103aa5afba309deee31868e8
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-18 12:55:05 +0100

    Create help_view.py

---
+++ views/help_view.py
@@ -0,0 +1,15 @@
+from django.http import FileResponse, Http404
+from omeroweb.decorators import login_required
+import os
+
+@login_required()
+def help_page(request, **kwargs):
+    help_path = "/opt/omero/web/OMERO.web/var/static/omeroweb_filenamemetadata/help.pdf"
+
+    if not os.path.exists(help_path):
+        raise Http404("Help file not found")
+
+    return FileResponse(
+        open(help_path, "rb"),
+        content_type="application/pdf"
+    )
