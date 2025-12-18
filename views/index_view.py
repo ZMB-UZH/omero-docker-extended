280d23dbe64670000e87e7acdc955372ea9e2ff9
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-18 12:19:54 +0100

    Update index_view.py

--- views/index_view.py
+++ views/index_view.py
@@ -192,7 +192,7 @@ def index(request, conn=None, url=None, **kwargs):
                             '>
                             <div style='min-width:300px;'>
                                 <label style="font-size:12px; display:block; margin-bottom:6px;">
-                                    Variable names: for unified guidelines see 
+                                    Variable names: for guidelines see 
                                     <a href="https://www.ebi.ac.uk/bioimage-archive/rembi-help-overview/"
                                        target="_blank"
                                        rel="noopener noreferrer"
