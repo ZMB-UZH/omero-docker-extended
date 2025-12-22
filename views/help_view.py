34de22b19fc49661a90c09559f0e1fe0ac0843c9
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-22 17:00:38 +0100

    Update help_view.py
    Signed-off-by: TechGuru7 <28989754+Stratos007@users.noreply.github.com>

--- views/help_view.py
+++ views/help_view.py
@@ -9,7 +9,7 @@ def help_page(request, **kwargs):
     help_path = os.path.join(
         base_dir,
         "static",
-        "omeroweb_filenamemetadata",
+        "omeroweb_zmb_plugin",
         "help.pdf",
     )
 
