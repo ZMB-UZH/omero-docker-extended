760e6f29a34f7d99632e11ac83aacdd66cd5f08b
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-22 16:56:11 +0100

    Update __init__.py
    Signed-off-by: TechGuru7 <28989754+Stratos007@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
-default_app_config = "omeroweb_filenamemetadata.apps.FilenameMetadataConfig"
+default_app_config = "omeroweb_zmb_plugin.apps.ZMBPluginConfig"
 
 from .views.index_view import index
 from .views.job_view import start_job, job_progress
-from .save_metadata_view import save_metadata
-
-
+from .save_keyvaluepairs_view import save_keyvaluepairs
