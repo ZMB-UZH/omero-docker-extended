f6e584f5e54e9bd62efba1bbf6a9d4cdefbc934f
strmt7 <28989754+strmt7@users.noreply.github.com>
2025-12-22 16:58:31 +0100

    Delete save_metadata_view.py
    Signed-off-by: TechGuru7 <28989754+Stratos007@users.noreply.github.com>

--- save_metadata_view.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from omeroweb.decorators import login_required
-
-@login_required()
-def save_metadata(request, conn=None, url=None, **kwargs):
-    return HttpResponse("Save endpoint ready")
-
-
