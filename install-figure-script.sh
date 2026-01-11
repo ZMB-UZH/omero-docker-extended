f5caf875220bb2790d21f35972c0165be9a5d18f
strmt7 <28989754+strmt7@users.noreply.github.com>
2026-01-12 00:07:28 +0100

    Update install-figure-script.sh
    Signed-off-by: strmt7 <28989754+strmt7@users.noreply.github.com>

--- install-figure-script.sh
+++ install-figure-script.sh
@@ -3,8 +3,15 @@ set -e
 echo "Checking for Figure_To_Pdf.py script..."
 if [ ! -f /opt/omero/server/OMERO.server/lib/scripts/omero/figure_scripts/Figure_To_Pdf.py ]; then
     echo "Script not found, installing..."
+    
+    # Detect installed OMERO.figure version
+    FIGURE_VERSION=$(/opt/omero/server/venv*/bin/pip show omero-figure | grep Version | cut -d' ' -f2)
+    echo "Detected OMERO.figure version: ${FIGURE_VERSION}"
+    
     mkdir -p /opt/omero/server/OMERO.server/lib/scripts/omero/figure_scripts
-    git clone --depth 1 https://github.com/ome/omero-figure.git /tmp/omero-figure
+    git clone --depth 1 --branch "v${FIGURE_VERSION}" https://github.com/ome/omero-figure.git /tmp/omero-figure || \
+        git clone --depth 1 https://github.com/ome/omero-figure.git /tmp/omero-figure
+    
     cp /tmp/omero-figure/omero_figure/scripts/omero/figure_scripts/Figure_To_Pdf.py \
         /opt/omero/server/OMERO.server/lib/scripts/omero/figure_scripts/Figure_To_Pdf.py
     chown -R omero-server:omero-server /opt/omero/server/OMERO.server/lib/scripts
