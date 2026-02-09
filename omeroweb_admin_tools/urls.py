91e41c980c4b6ca86a9d5b85b723213f6fb32dd8
strmt7 <28989754+strmt7@users.noreply.github.com>
2026-02-09 11:05:18 +0100

    Improve admin storage views and monitoring Grafana access

--- omeroweb_admin_tools/urls.py
+++ omeroweb_admin_tools/urls.py
@@ -5,6 +5,8 @@ from .views.index_view import (
     internal_log_labels,
     logs_data,
     logs_view,
+    grafana_proxy,
+    prometheus_proxy,
     resource_monitoring_data,
     resource_monitoring_view,
     root_status,
@@ -20,6 +22,8 @@ urlpatterns = [
     path("logs/internal-labels/", internal_log_labels, name="omeroweb_admin_tools_internal_labels"),
     path("resource-monitoring/", resource_monitoring_view, name="omeroweb_admin_tools_resource_monitoring"),
     path("resource-monitoring/data/", resource_monitoring_data, name="omeroweb_admin_tools_resource_monitoring_data"),
+    path("resource-monitoring/grafana-proxy/<path:subpath>", grafana_proxy, name="omeroweb_admin_tools_grafana_proxy"),
+    path("resource-monitoring/prometheus-proxy/<path:subpath>", prometheus_proxy, name="omeroweb_admin_tools_prometheus_proxy"),
     path("storage/", storage_view, name="omeroweb_admin_tools_storage"),
     path("storage/data/", storage_data, name="omeroweb_admin_tools_storage_data"),
 ]
