29fd97530d53a399359d5f423de2e2e13eb9c856
strmt7 <28989754+strmt7@users.noreply.github.com>
2026-03-04 08:37:07 +0100

    fix: Add app_name to urls.py to resolve VariableDoesNotExist error in OMERO web

--- omeroweb_imaris_connector/urls.py
+++ omeroweb_imaris_connector/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'omeroweb_imaris_connector'
+
 urlpatterns = [
     path("imaris-export/", views.imaris_export, name="imaris_export"),
 ]
